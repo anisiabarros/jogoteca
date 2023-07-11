0b5ff250cf198f0b18ae42e043b6718ac7cf8d13
anisiabarros <anisia.barros@gmail.com>
2023-07-11 01:50:02 -0300

    criando uma nova route para criar novos jogos para a lista

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,35 @@
 # flask-2.3.2
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 app = Flask(__name__)
 
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Figth', 'PS2')
+lista = [jogo1, jogo2, jogo3]
 @app.route('/inicio')
 def ola():
-    lista = ['Tetris', 'Scryrim', 'Crash']
+
 
     return render_template('lista.html', titulo= 'Jogos', jogos = lista)
 
-app.run()
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo = 'Novo Jogo')
+@app.route('/criar', methods = ['POST', ])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+app.run(debug=True)
