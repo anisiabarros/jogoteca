0d54eee628f1c646c72493de02a744ece7389490
anisiabarros <anisia.barros@gmail.com>
2023-07-18 01:32:29 -0300

    melhorando o codigo e a usabilidade

--- jogoteca.py
+++ jogoteca.py
@@ -1,5 +1,5 @@
 # flask-2.3.2
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 
 class Jogo:
@@ -8,14 +8,16 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
-app = Flask(__name__)
+
 
 jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
 jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
 jogo3 = Jogo('Mortal Kombat', 'Figth', 'PS2')
 lista = [jogo1, jogo2, jogo3]
-@app.route('/inicio')
-def ola():
+
+app = Flask(__name__)
+@app.route('/')
+def index():
 
 
     return render_template('lista.html', titulo= 'Jogos', jogos = lista)
@@ -23,13 +25,13 @@ def ola():
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo = 'Novo Jogo')
-@app.route('/criar', methods = ['POST', ])
+@app.route('/criar', methods = ['POST',])
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
-    return render_template('lista.html', titulo='Jogos', jogos=lista)
+    return redirect('/')
 
 app.run(debug=True)
